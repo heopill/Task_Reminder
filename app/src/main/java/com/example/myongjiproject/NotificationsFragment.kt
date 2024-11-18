aa31b1e5cfb6186be3fba83b54d138068b159d9d
heopill <heopill@naver.com>
2024-11-18 20:55:42 +0900

    feat : NotificationsFragment 구현
    WebView를 이용해 NotificationsFragment에
    명지전문대학 공지사항 홈페이지 구현

--- app/src/main/java/com/example/myongjiproject/NotificationsFragment.kt
+++ app/src/main/java/com/example/myongjiproject/NotificationsFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myongjiproject.databinding.FragmentNotificationsBinding
 
 
 class NotificationsFragment : Fragment() {
+
+    private lateinit var binding: FragmentNotificationsBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_notifications, container, false)
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://www.mjc.ac.kr/bbs/data/list.do?menu_idx=66") // 학교 공지사항 URL
+
+        return binding.root
     }
 }
